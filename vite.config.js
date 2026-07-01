1fd932d09cdf2b7b0f65e445d3de43601307d4ad
mahmudzahin <mahmudzahin327@gmail.com>
2026-07-01 22:07:32 +0600

    Configure Deployment settings

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base:'/Car_gallery',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
